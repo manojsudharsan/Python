52389e270e73538591818f056cecf64055f26fec
manojsudharsan <manojsudharsanbe@gmail.com>
2019-06-12 16:05:14 +0530

    Update CheckNumberAtleastOne.py

--- CheckNumberAtleastOne.py
+++ CheckNumberAtleastOne.py
@@ -4,7 +4,7 @@ D=list(map(int,input().split()))
 e=0
 for i in range(0,X):
       for j in range(0,Y):
-            if C[i]==D[i]:
+            if C[i]==D[j]:
                 e=e+1
 if e>1:
     print("YES")
