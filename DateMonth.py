542e63b68a73b3bdc7c7b53eb734e36a26462fe0
manojsudharsan <manojsudharsanbe@gmail.com>
2019-06-13 16:19:48 +0530

    Create DateMonth.py

---
+++ DateMonth.py
@@ -0,0 +1,25 @@
+X,Y,Z=list(map(int,input().split("-")))
+if(Y==1):
+    print("January")
+elif(Y==2):
+    print("February")
+elif(Y==3):
+    print("March")
+elif(Y==4):
+    print("April")
+elif(Y==5):
+    print("May")
+elif(Y==6):
+    print("June")
+elif(Y==7):
+    print("July")
+elif(Y==8):
+    print("August")
+elif(Y==9):
+    print("September")
+elif(Y==10):
+    print("October")
+elif(Y==11):
+    print("November")
+elif(Y==12):
+    print("December")
