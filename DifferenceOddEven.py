98b174dcc6b220493484e59d1d547b87aad18e18
manojsudharsan <manojsudharsanbe@gmail.com>
2019-06-28 18:00:16 +0530

    Update DifferenceOddEven.py

--- DifferenceOddEven.py
+++ DifferenceOddEven.py
@@ -1,11 +1,11 @@
 X,Y=map(int,input().split())
 if(X>=Y):
-  if((X-Y)%2==0) or X-Y==0):
+  if((X-Y)%2==0 or X-Y==0):
       print("even")
   else:
       print("odd")
 elif(Y>=X):
-  if((Y-X)%2==0) or Y-X==0):
+  if((Y-X)%2==0 or Y-X==0):
       print("even")
   else:
       print("odd")
