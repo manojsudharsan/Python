a3b109b4fdb124deaea8f22a6c04e487b6cccfad
manojsudharsan <manojsudharsanbe@gmail.com>
2019-07-01 16:36:08 +0530

    Update Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -1,10 +1,13 @@
 s='dhoni'
 x=str(input())
 c=0
-for i in range(0,len(s)):
-    if x[i] in s:
-      c=c+1
-if(c==5):
-  print("yes")
+if(len(x)==5):
+    for i in range(0,len(s)):
+        if x[i] in s:
+            c=c+1
+    if(c==5):
+        print("yes")
+    else:
+        print("no")
 else:
-  print("no")
+    print("no")
