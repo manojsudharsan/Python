73fc37775ba893a0e113d08b9490085a5fa86cf8
manojsudharsan <manojsudharsanbe@gmail.com>
2019-07-13 14:32:35 +0530

    Create SortString.py

---
+++ SortString.py
@@ -0,0 +1,12 @@
+X=int(input())
+Y=list(map(str,input().split()))[:X]
+l=[]
+t=[]
+for i in range(0,len(Y)):
+    l.append(Y[i].lower())
+for i in range(0,X):
+      m=min(l)
+      t.append(m)
+      l.remove(min(l))
+for i in t:
+    print(i)
