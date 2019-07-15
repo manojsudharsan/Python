d98b6fc2a3feb81b6184aa2ae50d1e281c0de158
manojsudharsan <manojsudharsanbe@gmail.com>
2019-07-15 14:53:08 +0530

    Update SumofMax.py

--- SumofMax.py
+++ SumofMax.py
@@ -1,4 +1,10 @@
 X=int(input())
 Y=list(map(int,input().split()))[:X]
-Y.remove(min(Y))
-print(sum(Y))
+t=[]
+for i in range(0,X-1):
+   if(Y[i]>Y[i+1]):
+      t.append(Y[i])
+   else:
+      t.append(Y[i+1])
+print(sum(t))
+  
