e3eff26dbf850b24487c91331b69bc4e4079ee8c
manojsudharsan <manojsudharsanbe@gmail.com>
2019-07-16 14:49:22 +0530

    Update PrefixSumArray.py

--- PrefixSumArray.py
+++ PrefixSumArray.py
@@ -2,10 +2,13 @@ X=int(input())
 Y=list(map(int,input().split()))[:X]
 t=[]
 for i in range(0,X):
-    i=0
-    e=sum(Y)
-    t.append(e)
-    Y.remove(Y[i])
-t=t[::-1]
+     n=Y[0:i+1]
+     if(sum(n)%2==0):
+        t.append(str(sum(n)))
+     else:
+        t.append(str(Y[i]))
 print(*t,sep=" ")
+       
+     
+
        
